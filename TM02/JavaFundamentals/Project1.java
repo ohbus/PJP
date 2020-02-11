fc1218b9740cda6c9165ceee42c88cc89e22ea06
Subhrodip Mohanta <subhrodipmohanta@gmail.com>
2020-02-11 22:43:25 +0530

    WIP : TM02

---
+++ TM02/JavaFundamentals/Project1.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package JavaFundamentals;
+
+/**
+ * @author subho
+ *
+ */
+public class Project1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
+
+class Employee	{
+	
+	int empNo;
+	String empName;
+	String date;
+	char desCode;
+	String dept;
+	long basic;
+	long hra;
+	long it;
+	
+	Employee()	{
+		empNo = "";
+		empName = "";
+		date = "";
+		desCode = '';
+		dept = "";
+		basic = 0;
+		hra = 0;
+		it = 0;
+	}
+	
+	Employee() {
+		
+	}
+}
---
+++ TM02/JavaFundamentals/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author subho
+ *
+ */
+package JavaFundamentals;
