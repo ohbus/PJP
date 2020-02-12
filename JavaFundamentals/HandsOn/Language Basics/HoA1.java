083c92e450b8376ca08f31989f6ba643e89b0b50
Souph <souptik43.hari@gmail.com>
2020-02-12 20:55:09 +0530

    WIP: HandsOn

---
+++ JavaFundamentals/HandsOn/Language Basics/HoA1.java
@@ -0,0 +1,9 @@
+
+public class HoA1 {
+
+	public static void main(String[] args) {
+		System.out.println(args[0]+" "+"Technologies"+" "+args[1]);
+
+	}
+
+}
---
+++ JavaFundamentals/HandsOn/Language Basics/HoA2.java
@@ -0,0 +1,9 @@
+
+public class HoA2 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome "+args[0]);
+
+	}
+
+}
