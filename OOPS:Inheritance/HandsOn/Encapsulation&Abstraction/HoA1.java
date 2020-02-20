6a1096d0b6c7ea3b4573d67b6a231c008163ed5e
Souph <souptik43.hari@gmail.com>
2020-02-20 22:16:24 +0530

    OOPs_Encapsulation&Abstraction_HandsOn

--- OOPS:Inheritance/HandsOn/Encapsulation&Abstraction/HoA1.java
+++ OOPS:Inheritance/HandsOn/Encapsulation&Abstraction/HoA1.java
@@ -28,19 +28,25 @@ class Author
 class Book
 {
 	public String name;
-	public Double price;
+	public double price;
 	public int qtyInStock;
+	public String AuthorName;
+	String email;
+	char gender;
 	Author a;
 	public Book(String name,double price,int qtyInStock,String AuthorName,String email,char gender)
 	{
 		this.name = name;
 		this.price = price;
 		this.qtyInStock = qtyInStock;
-		a = new Author("AuthorName","email",gender);
+		a = new Author(AuthorName,email,gender);
 	}
-	public String AuthorName = a.getName();
-	public String email = a.getEmail();
-	public char gender = a.getGender();
+	public void getData()
+	{
+		AuthorName = a.getName();
+		email = a.getEmail();
+		gender = a.getGender();
+	}
 }
 
 
@@ -49,6 +55,7 @@ public class HoA1
 	public static void main(String[] args)
 	{
 		Book b = new Book("ABC",450,15,"XYZ","XYZ@mail.com",'M');
+		b.getData();
 		System.out.println(b.name+"\t"+b.price+"\t"+b.qtyInStock+"\t"+b.AuthorName+"\t"+b.email+"\t"+b.gender);
 	}
 }
